cab2619137c5b574c46d8b313dd7daee9b8c1d68
adharshkc <adharshkc2017@gmail.com>
2025-12-19 13:54:41 +0530

    updated drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL, ensure the database is provisioned");
+// }
 
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: "postgresql://neondb_owner:npg_i6drDtCZa7YV@ep-weathered-violet-a45avwu1-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 });
